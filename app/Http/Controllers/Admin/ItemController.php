09b0a5a4ed79de4c60dd73f214521c6701c1d970
Slowpoked <slowpokedme@gmail.com>
2021-01-13 19:40:49 +0500

    Disable aspect ratio

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -146,7 +146,7 @@ class ItemController extends CrudController
             'name' => "image",
             'type' => 'image',
             'crop' => true, // set to true to allow cropping, false to disable
-            'aspect_ratio' => 1, // ommit or set to 0 to allow any aspect ratio
+            'aspect_ratio' => 0, // ommit or set to 0 to allow any aspect ratio
             'disk'  => 'public', // in case you need to show images from a different disk
 //            'prefix'    => 'uploads/categories' // in case your db value is only the file name (no path), you can use this to prepend your path to the image src (in HTML), before it's shown to the user;
         ]);
