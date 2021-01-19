5eb498ba6979775a2768a17fd64ad6d45603f1fe
Slowpoked <slowpokedme@gmail.com>
2021-01-19 17:43:55 +0500

    UX improve

--- resources/views/cash/components/item-single.blade.php
+++ resources/views/cash/components/item-single.blade.php
@@ -47,7 +47,7 @@
                             </svg>
                         </button>
                         <span class="bg-purple-800 text-white font-bold py-2 px-3">
-                            {{ $cart_items[$item->id]['quantity'] }}
+                            {{ trans('custom.x_quantity', ['count' => $cart_items[$item->id]['quantity']]) }}
                         </span>
                         @if($cart_items[$item->id]['quantity'] < $item->stock)
                             <button wire:click="add({{ $item->id }})" class="focus:outline-none bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2">
@@ -58,11 +58,11 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                             </svg>
                         </button>
-                        <button wire:click="remove({{ $item->id }})" class="focus:outline-none ml-2 bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-2 shadow inline-flex items-center">
-                            <svg class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
+                        {{--<button wire:click="remove({{ $item->id }})" class="focus:outline-none ml-2 bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-2 shadow inline-flex items-center">--}}
+                            {{--<svg class="fill-current w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">--}}
+                                {{--<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />--}}
+                            {{--</svg>--}}
+                        {{--</button>--}}
                         @else
                             <button wire:click="add({{ $item->id }})" class="focus:outline-none bg-purple-800 hover:bg-indigo-600 text-white font-bold py-2 px-2 shadow inline-flex items-center">
                                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
